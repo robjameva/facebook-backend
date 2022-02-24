5375411f46801c5d58bc19c25a1b19437fc76ae4
Robert Evanik <rob.evanik@gmail.com>
2022-02-24 17:55:47 -0500

    Finished schemas

--- models/Thought.js
+++ models/Thought.js
@@ -1,19 +1,20 @@
 const { Schema, model, Types } = require('mongoose')
 const dateFormat = require('../utils/dateFormat')
 
-const ReplySchema = new Schema(
+const ReactionSchema = new Schema(
     {
-        // set custom id to avoid confusion with parent comment _id
-        replyId: {
+        // set custom id to avoid confusion with parent thought _id
+        reactionID: {
             type: Schema.Types.ObjectId,
             default: () => new Types.ObjectId()
         },
-        replyBody: {
+        reactionBody: {
             type: String,
             required: true,
-            trim: true
+            trim: true,
+            maxlength: 280
         },
-        writtenBy: {
+        username: {
             type: String,
             required: true,
             trim: true
@@ -22,33 +23,30 @@ const ReplySchema = new Schema(
             type: Date,
             default: Date.now,
             get: createdAtVal => dateFormat(createdAtVal)
-        }
-    },
-    {
-        toJSON: {
-            getters: true
         }
     }
 );
 
-const CommentSchema = new Schema(
+const ThoughtSchema = new Schema(
     {
-        writtenBy: {
+        thoughtText: {
             type: String,
             required: true,
-            trim: true
-        },
-        commentBody: {
-            type: String,
-            required: true,
-            trim: true
+            minlength: 1,
+            maxlength: 280
         },
         createdAt: {
             type: Date,
             default: Date.now,
             get: createdAtVal => dateFormat(createdAtVal)
         },
-        replies: [ReplySchema]
+        username: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ],
+        reactions: [ReactionSchema]
     },
     {
         toJSON: {
@@ -58,12 +56,17 @@ const CommentSchema = new Schema(
     }
 );
 
-CommentSchema.virtual('replyCount').get(function() {
-    return this.replies.length;
+
+ThoughtSchema.virtual('username').get(function() {
+    return this.parent().username
+})
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
 })
 
 // create the Comment model using the CommentSchema
-const Comment = model('Comment', CommentSchema);
+const Thought = model('Thought', ThoughtSchema);
 
 // export the Comment model
-module.exports = Comment;
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose')
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+        }
+    ],
+    firends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+// create the user model using the userSchema
+const User = model('User', UserSchema);
+
+// export the user model
+module.exports = User;
